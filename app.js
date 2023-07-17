c3815b14fe4a78afdba29ee6d299c46534197a03
ahmedJamal <ajamalselim@gamail.com>
2023-07-18 00:10:46 +0300

    fixing stripe

--- app.js
+++ app.js
@@ -49,7 +49,14 @@ app.post(
 );
 
 // body parser: reading data from the body to req.body
-app.use(express.json({ limit: '10kb' }));
+app.use((req, res, next) => {
+  if (req.originalUrl === '/webhook-checkout') {
+    next();
+  } else {
+    express.json({ limit: '10kb' })(req, res, next);
+  }
+});
+// app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
 // adding requested at
